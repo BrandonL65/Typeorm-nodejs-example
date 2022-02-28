223d549daa4fd805249e4b7387a852c112200da1
Brandon Lau <brandonlau@Brandons-MacBook-Pro.local>
2022-02-28 03:06:38 -0500

    more examples

--- src/index.ts
+++ src/index.ts
@@ -9,15 +9,29 @@ const starting = async () => {
   const userRepository = connection.getRepository(User);
 
   newUser.age = "26";
-  newUser.name = "john";
+  newUser.name = "example";
 
   await userRepository.save(newUser);
 
-  const foundUser = await userRepository.find({
-    where: [{ name: newUser.name }],
-  });
+  const foundUser = await userRepository
+    .createQueryBuilder()
+    .select("user")
+    .from(User, "user")
+    .where("user.name = :name", { name: newUser.name })
+    .getOne();
 
   console.log(`User created and found, `, foundUser);
+
+  foundUser.age = "27";
+
+  await userRepository.save(foundUser);
+
+  const updatedUser = await userRepository
+    .createQueryBuilder("user")
+    .where("user.name = :name", { name: foundUser.name })
+    .getMany();
+
+  console.log("user found after updating", updatedUser);
 };
 
 starting();
